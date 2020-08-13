9a9c7ec668b8c3e3ad99c064993351597d3d9062
Georjane <pzgeorjane@gmail.com>
2020-08-13 14:52:33 +0100

    Layout space inside hash literal fix

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,5 +3,5 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   validates :body, presence: true
-  validates :user_id, :post_id, presence: true, numericality: {only_integer: true}
+  validates :user_id, :post_id, presence: true, numericality: { only_integer: true }
 end
