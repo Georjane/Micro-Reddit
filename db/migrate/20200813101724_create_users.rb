cb2de60b027f900ba51b320a1e7c39ed4130062c
Georjane <pzgeorjane@gmail.com>
2020-08-13 11:36:39 +0100

    Add name, email and password colunm to user table

--- db/migrate/20200813101724_create_users.rb
+++ db/migrate/20200813101724_create_users.rb
@@ -1,6 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :password
 
       t.timestamps
     end
