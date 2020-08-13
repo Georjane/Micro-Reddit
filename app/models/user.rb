ba10a3c56e711b0deafc62afd85cc04a5c07f804
Georjane <pzgeorjane@gmail.com>
2020-08-13 12:08:21 +0100

    Add validation for user colunms

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
-  
+  validates :username, presence: true, uniqueness: true, length: {in: 3..10}
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: {in: 5..15}
 end
